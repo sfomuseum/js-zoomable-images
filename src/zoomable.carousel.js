839be94cb5b47b9396bbc19c9aaaf3b80c3f9af8
thisisaaronland <thisisaaronland@localhost>
2021-08-03 12:25:08 -0700

    update to support location.hash indicators; update onclick function

--- src/zoomable.carousel.js
+++ src/zoomable.carousel.js
@@ -90,11 +90,14 @@ zoomable.carousel = (function(){
 		img_node.setAttribute("data-index", j);		
 		img_node.setAttribute("src", img_src);
 
-		var link = _links[img_src];
-
 		if (link){
-		    img_node.onclick = function(){
-			location.href = link;
+		    img_node.onclick = function(e){
+			var el = e.target;
+			var src = el.getAttribute("src");
+			var attrs = _attrs[src];
+			var id = attrs["image-id"];
+			self.assign(id);
+			return false;
 		    };
 		}
 
@@ -113,6 +116,16 @@ zoomable.carousel = (function(){
 	    advance_el.onclick = self.advance;
 	    _carousel.appendChild(advance_el);
 
+	    // auto-load a specific image based on the presence of its ID in location.hash
+
+	    if (location.hash != ""){
+
+		var hash = location.hash.substr(1);
+		var id = hash;
+
+		self.assign(id);
+	    }
+	    
 	    self.show();
 	},
 
@@ -142,7 +155,88 @@ zoomable.carousel = (function(){
 	'hide': function(){
 	    _carousel.style.display = "none";
 	},
-	
+
+	'assign': function(id){
+
+	    // console.log("ASSIGN", id);
+
+	    var visible = document.getElementsByClassName("zoomable-carousel-visible");
+	    var count_visible = visible.length;
+
+	    var current_idx = self.index_for_center();
+	    var new_idx = self.index_for_id(id);
+
+	    if (new_idx == -1){
+		console.log("Can't determine new index");
+		return false;
+	    }
+
+	    var current_el = visible[current_idx];
+	    var current_src = current_el.getAttribute("src");
+	    var current_attrs = _attrs[current_src];
+
+	    var new_src = _images[new_idx];
+	    var new_attrs = _attrs[new_src];
+
+	    self.update(current_attrs, new_attrs);
+
+	    // now update the bookends
+
+	    var count_images = _images.length;
+
+	    var center_el = visible[current_idx];
+	    center_el.setAttribute("src", new_src);
+	    center_el.setAttribute("data-index", new_idx);
+
+	    for (var i=0; i < current_idx; i++){
+		
+		var j = i + 1;
+
+		var prev_idx = (new_idx == 0) ? count_images - 1 : new_idx - j;
+		var prev_src = _images[prev_idx];
+
+		var prev_el = visible[i];
+		prev_el.setAttribute("src", prev_src);
+		prev_el.setAttribute("data-index", prev_idx);
+	    }
+
+	    for (var i= (current_idx + 1); i < count_visible; i++){
+
+		var next_idx = (new_idx == (count_images - 1)) ? 0 : new_idx + 1;
+		var next_src = _images[next_idx];
+
+		var next_el = visible[i];
+		next_el.setAttribute("src", next_src);
+		next_el.setAttribute("data-index", next_idx);
+	    }
+	},
+
+	'index_for_center': function(){
+	    var count = _images.length;
+	    var center = Math.floor(count / 2);
+	    return center;
+	},
+
+	'index_for_id': function(id){
+
+	    var idx = -1;
+
+	    var count = _images.length;
+
+	    for (var i=0; i < count; i++){
+
+		var src = _images[i];
+		var attrs = _attrs[src];
+
+		if (attrs["image-id"] == id){
+		    idx = i;
+		    break;
+		}
+	    }
+
+	    return idx;
+	},
+
 	'advance': function(e){
 
 	    var count_images = _images.length;
@@ -182,7 +276,7 @@ zoomable.carousel = (function(){
 	    var visible = document.getElementsByClassName("zoomable-carousel-visible");
 	    var count = visible.length;
 
-	    var center = Math.floor(count / 2);	// AGAIN, account for image count of 2
+	    var center = Math.floor(count / 2);
 
 	    for (var i=0; i < count; i++){
 		
@@ -207,8 +301,11 @@ zoomable.carousel = (function(){
 	},
 	
 	'update': function(current_attrs, updated_attrs){
-	    console.log("update", current_attrs, updated_attrs);
 
+	    // console.log("CURRENT", current_attrs);
+	    // console.log("UPDATED", updated_attrs);
+
+	    location.hash = updated_attrs["image-id"];
 
 	    var updated_zoomable = zoomable.builder.make_zoomable_element(updated_attrs);
 	    var current_zoomable = document.getElementById("zoomable-image-" + current_attrs["image-id"]);
